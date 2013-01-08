9ec23bffa9e8aa82bf62032ee0565bbd912dec19
dfdun <dfdun@ingenic.cn>
2013-01-08 18:16:08 +0800

    modify UpdateInstallActivity and some string, etc

--- src/cn/ingenic/updater/UpdateInstallActivity.java
+++ src/cn/ingenic/updater/UpdateInstallActivity.java
@@ -16,21 +16,25 @@ public class UpdateInstallActivity extends Activity implements OnClickListener {
 	private File mUpdateFile;
 	private TextView mText;
 	private boolean mSaveFile;
+	private boolean mClicked;
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_update_install);
-	    mSaveFile = false;
+	    mSaveFile = true;
+	    mClicked = false;
 	    String file_path = getIntent().getStringExtra("update_file");
-	    String description = getIntent().getStringExtra("description");
-	    String note = this.getString(R.string.install_description);
+	    UpdateInfo info = (UpdateInfo)getIntent().getParcelableExtra("update_info");
 	    mUpdateFile = new File(file_path.substring(file_path.indexOf("/sdcard")));
 	    findViewById(R.id.abandon).setOnClickListener(this);
 	    findViewById(R.id.later).setOnClickListener(this);
 	    findViewById(R.id.install).setOnClickListener(this);
-	    mText = (TextView)findViewById(R.id.update_description);
-        mText.setText(description==null?"":description + "\n" + note);
+	    mText = (TextView)findViewById(R.id.update_info);
+
+        mText.setText(getString(R.string.version) + info.version + " ( "
+                + info.size / 1024 / 1024 + " MB )\n"
+                + getString(R.string.description) + info.description + "\n");
 	}
 
 	@Override
@@ -38,17 +42,19 @@ public class UpdateInstallActivity extends Activity implements OnClickListener {
 		switch (view.getId()) {
 		case R.id.abandon:
 		    mSaveFile = false;
+		    mClicked = true;
 			break;
 		case R.id.later:
 		    mSaveFile = true;
+		    mClicked = true;
 		    updateLater();
 			break;
 		case R.id.install:
 		    mSaveFile = true;
+		    mClicked = true;
 			try {
 				RecoverySystem.installPackage(this, mUpdateFile);
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			break;
@@ -56,12 +62,16 @@ public class UpdateInstallActivity extends Activity implements OnClickListener {
 		this.finish();
 	}
 
-	@Override
-	public void onStop(){
-	    super.onStop();
-	    if (!mSaveFile && mUpdateFile != null && mUpdateFile.exists())
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (!mSaveFile && mUpdateFile != null && mUpdateFile.exists()) {
             mUpdateFile.delete();
-	}
+        }
+        if (!mClicked) {
+            updateLater();
+        }
+    }
 
     private void updateLater() {
         long when = System.currentTimeMillis();
@@ -69,7 +79,7 @@ public class UpdateInstallActivity extends Activity implements OnClickListener {
         CharSequence contentText = getText(R.string.update_later_msg);
         Notification.Builder builder=new Notification.Builder(this);
         builder.setSmallIcon(R.drawable.ic_launcher)
-        .setTicker(getString(R.string.install_alerting))
+        .setTicker(getString(R.string.update_later_msg))
         .setWhen(when+100)
         .setContentTitle(contentTitle)
         .setContentText(contentText)
